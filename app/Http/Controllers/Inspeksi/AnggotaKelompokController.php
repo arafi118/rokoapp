8f6422f5f70ebcf22f83efeeecc9ed50faf5f807
nurfandi31 <fandinur241@gmail.com>
2025-08-05 15:16:27 +0700

    controller

---
+++ app/Http/Controllers/Inspeksi/AnggotaKelompokController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Anggota_kelompok;
+use App\Http\Requests\StoreAnggota_kelompokRequest;
+use App\Http\Requests\UpdateAnggota_kelompokRequest;
+
+class AnggotaKelompokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnggota_kelompokRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Anggota_kelompok $anggota_kelompok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Anggota_kelompok $anggota_kelompok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnggota_kelompokRequest $request, Anggota_kelompok $anggota_kelompok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Anggota_kelompok $anggota_kelompok)
+    {
+        //
+    }
+}
