0187aa3788afd732d265a917501d480035307181
nurfandi31 <fandinur241@gmail.com>
2025-08-14 09:45:29 +0700

    tb absensi

--- database/migrations/2025_08_14_015457_create_absensi_table.php
+++ database/migrations/2025_08_14_015457_create_absensi_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('absensi', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->foreignId('karyawan_id')->constrained('karyawan');
+            $table->foreignId('group_id')->constrained('group');
             $table->string('jadwal')->nullable();
             $table->date('tanggal')->nullable();
             $table->time('jam')->nullable();
