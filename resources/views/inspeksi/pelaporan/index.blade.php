f52924f881cf9d964ae566a76e86457e90568d32
farilraehan <farilraihan50@gmail.com>
2025-10-23 09:45:15 +0700

    penambahan  periode / thn bulan tgl laporan

--- resources/views/inspeksi/pelaporan/index.blade.php
+++ resources/views/inspeksi/pelaporan/index.blade.php
@@ -43,35 +43,81 @@
         <div class="card-body">
             <form action="/inspeksi/pelaporan/preview" method="GET" target="_blank">
                 <div id="laporanRow" class="row g-3 align-items-end mt-2">
-                    {{-- Nama Laporan --}}
-                    <div id="colLaporan" class="col-md-6">
-                        <label class="form-label">Nama Laporan</label>
-                        <select id="laporan" name="laporan" class="form-select select2">
-                            <option value="">---</option>
-                            @foreach ($laporan as $item)
-                                <option value="{{ $item->file }}"
-                                    {{ request('laporan') == $item->file ? 'selected' : '' }}>
-                                    {{ $item->nama_laporan }}
-                                </option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3">
+                        {{-- Tahun --}}
+                        <div class="col-md-4">
+                            <label class="form-label">Tahunan</label>
+                            <select name="tahun" class="form-select select2">
+                                @for ($i = 2020; $i <= date('Y'); $i++)
+                                    <option value="{{ $i }}" {{ $i == date('Y') ? 'selected' : '' }}>
+                                        {{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+
+                        {{-- Bulan --}}
+                        <div class="col-md-4">
+                            <label class="form-label">Bulanan</label>
+                            <select name="bulan" class="form-select select2">
+                                @foreach ([
+            '01' => 'JANUARI',
+            '02' => 'FEBRUARI',
+            '03' => 'MARET',
+            '04' => 'APRIL',
+            '05' => 'MEI',
+            '06' => 'JUNI',
+            '07' => 'JULI',
+            '08' => 'AGUSTUS',
+            '09' => 'SEPTEMBER',
+            '10' => 'OKTOBER',
+            '11' => 'NOVEMBER',
+            '12' => 'DESEMBER',
+        ] as $num => $name)
+                                    <option value="{{ $num }}" {{ $num == date('m') ? 'selected' : '' }}>
+                                        {{ $num }}.
+                                        {{ $name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-4 mb-3" style="padding-right: 10px;">
+                            <label class="form-label">Harian</label>
+                            <select name="hari" class="form-select select2">
+                                <option value="">---</option>
+                                @for ($i = 1; $i <= 31; $i++)
+                                    <option value="{{ $i }}">{{ str_pad($i, 2, '0', STR_PAD_LEFT) }}</option>
+                                @endfor
+                            </select>
+                        </div>
+
+                        {{-- Nama Laporan --}}
+                        <div id="colLaporan" class="col-md-6 mb-3" style="padding-left: 10px;">
+                            <label class="form-label">Nama Laporan</label>
+                            <select id="laporan" name="laporan" class="form-select select2">
+                                <option value="">---</option>
+                                @foreach ($laporan as $item)
+                                    <option value="{{ $item->file }}"
+                                        {{ request('laporan') == $item->file ? 'selected' : '' }}>
+                                        {{ $item->nama_laporan }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        {{-- Sub Laporan --}}
+                        <div id="subLaporan" class="col-md-6">
+                            <label class="form-label">Nama Sub Laporan</label>
+                            <select name="sub_laporan" id="sub_laporan" class="form-select select2">
+                                <option value="">---</option>
+                            </select>
+                        </div>
                     </div>
 
-                    {{-- Sub Laporan --}}
-                    <div id="subLaporan" class="col-md-6">
-                        <label class="form-label">Nama Sub Laporan</label>
-                        <select name="sub_laporan" id="sub_laporan" class="form-select select2">
-                            <option value="">---</option>
-                        </select>
+                    <div class="col-12">
+                        <div class="d-flex justify-content-end gap-2 mt-3">
+                            <button type="submit" name="action" value="excel" class="btn btn-success">Excel</button>
+                            <button type="submit" name="action" value="preview" class="btn btn-primary">Preview</button>
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-12">
-                    <div class="d-flex justify-content-end gap-2 mt-3">
-                        <button type="submit" name="action" value="excel" class="btn btn-success">Excel</button>
-                        <button type="submit" name="action" value="preview" class="btn btn-primary">Preview</button>
-                    </div>
-                </div>
             </form>
         </div>
     </div>
